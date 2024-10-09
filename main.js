824372004a307447b78525877ec28523d453f7f8
Alaric <alaric.henrot@gmail.com>
2024-10-09 17:05:59 +0200

    finish convertion functionality and add favicon

--- main.js
+++ main.js
@@ -1,33 +1,69 @@
 import './style.css';
-
-
 import axios from "axios";
 
 
 const getCurrencies = async () => {
 
-    try {
-        const response = await axios.get(`https://api.freecurrencyapi.com/v1/currencies?apikey=${import.meta.env.VITE_API_KEY}`);
-        return response.data.data;
-    } catch (error) {
-        return console.error('Error:', error);
-    }
-}
+  try {
+    const response = await axios.get(`https://api.freecurrencyapi.com/v1/currencies?apikey=${import.meta.env.VITE_API_KEY}`);
+    return response.data.data;
+  } catch (error) {
+    return console.error('Error:', error);
+  }
+};
 
 const selectToConvert = document.getElementById("currencyToConvert")
-const selectConverted = document.getElementById("conertedCurrency")
+const selectConverted = document.getElementById("convertedCurrency")
 
 
 const mapCurrencies = async (select) => {
-    const currencies = Object.values(await getCurrencies());
-    currencies.forEach(currency => {
-        const option = new Option(currency.symbol, currency.symbol)
-        select.appendChild(option)
+  const currencies = Object.values(await getCurrencies());
+  currencies.forEach(currency => {
+    const option = new Option(currency.symbol, currency.code)
+    select.appendChild(option)
 
-    })
+  })
 
 
-}
+};
 
 mapCurrencies(selectToConvert);
 mapCurrencies(selectConverted);
+
+
+
+const getUserCurrency = (selected) => {
+
+  return selected.value;
+
+};
+
+const getCurrenciesRatio = async (targetCurrency, sourceCurrency) => {
+
+  try {
+    const response = await axios.get(`https://api.freecurrencyapi.com/v1/latest?apikey=${import.meta.env.VITE_API_KEY}&currencies=${targetCurrency}&base_currency=${sourceCurrency}`);
+    return response.data.data;
+  } catch (error) {
+    return console.error('Error:', error);
+  }
+};
+
+const form = document.getElementById("convertionForm");
+
+const handleconvert = async () => {
+  let targetCurrency = getUserCurrency(selectConverted);
+  let sourceCurrency = getUserCurrency(selectToConvert);
+  const resultDiv = document.getElementById("result");
+  const amount = document.getElementById("amount").value;
+  const ratio = await getCurrenciesRatio(targetCurrency, sourceCurrency);
+
+  resultDiv.innerText = ratio[targetCurrency] * amount;
+
+};
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault;
+
+  handleconvert();
+})
+
